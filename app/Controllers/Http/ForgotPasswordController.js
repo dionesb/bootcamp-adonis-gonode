e70614c3d58291eaa4613e8a172caee441bc293d
Diones Breda <diones.breda@gmail.com>
2020-02-11 08:50:22 -0300

    feat: mail sending

--- app/Controllers/Http/ForgotPasswordController.js
+++ app/Controllers/Http/ForgotPasswordController.js
@@ -2,6 +2,7 @@
 
 const crypto = require('crypto')
 const User = use('App/Models/User')
+const Mail = use('Mail')
 
 class ForgotPasswordController {
   async store ({ request, response }) {
@@ -15,6 +16,20 @@ class ForgotPasswordController {
       user.token_created_at = new Date()
 
       await user.save()
+
+      await Mail.send(
+        ['emails.forgot_password'],
+        {
+          email,
+          token: user.token,
+          link: `${request.input('redirect_url')}?token=${user.token}`
+        },
+        message => {
+          message
+            .to(user.email)
+            .from('gonode@gonode.com', 'GoNode!!!')
+            .subject('Recuperação de senha')
+        })
     } catch (error) {
       return response
         .status(error.status)
