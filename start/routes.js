152ec94c0bac38e420897de7432609d3794a5a27
Diones Breda <diones.breda@gmail.com>
2020-02-11 22:30:13 -0300

    feat: projects CRUD

--- start/routes.js
+++ start/routes.js
@@ -7,9 +7,12 @@ Route.post('/users', 'UserController.store')
 Route.post('/sessions', 'SessionController.store')
 
 Route.post('/passwords', 'ForgotPasswordController.store')
-
 Route.put('/passwords', 'ForgotPasswordController.update')
 
 Route.get('/files/:id', 'FileController.show')
 
-Route.post('/files', 'FileController.store')
+Route.group(() => {
+  Route.post('/files', 'FileController.store')
+
+  Route.resource('/projects', 'ProjectController').apiOnly()
+}).middleware(['auth'])
